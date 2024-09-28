b24f213f5a2ef76e2c4e9b2ee83c327bd5618076
Elbert Nathanael <elbertwithane@gmail.com>
2024-09-28 13:52:58 +0800

    added dev mode and dev bot

--- src/environmentVariables.ts
+++ src/environmentVariables.ts
@@ -1,11 +1,24 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
-// api keys and credentials
-const constants = {
-    discordApiKey: process.env.DISCORD_API_KEY,
-    discordClientId: process.env.DISCORD_CLIENT_ID,
-    openAIKey: process.env.OPEN_AI_KEY,
-    stinkeeNotesChannel: process.env.STINKEE_NOTES_CHANNEL_ID
+
+const productionMode = process.env.MODE === "production";
+
+if (productionMode) {
+  console.log("❗❗ --- Starting in production mode --- ❗❗");
+} else {
+  console.log("--- Starting in development mode ---");
 }
 
-export default constants
+// api keys and credentials
+const constants = {
+  discordApiKey: productionMode
+    ? process.env.DISCORD_API_KEY
+    : process.env.DEV_DISCORD_API_KEY,
+  discordClientId: productionMode
+    ? process.env.DISCORD_CLIENT_ID
+    : process.env.DEV_DISCORD_CLIENT_ID,
+  openAIKey: process.env.OPEN_AI_KEY,
+  stinkeeNotesChannel: process.env.STINKEE_NOTES_CHANNEL_ID,
+};
+
+export default constants;
